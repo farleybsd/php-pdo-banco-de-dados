7301b49986c1002caf73b76ca8c72c1476abb844
farleybsd <farley.t.i@hotmail.com>
2020-05-20 18:11:22 -0300

    Modos de busca

--- php-pdo-projeto-inicial/Lista-Alunos.php
+++ php-pdo-projeto-inicial/Lista-Alunos.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
 
 require 'vendor/autoload.php';
 
@@ -7,4 +8,16 @@ $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 $statement = $pdo->query('SELECT * FROM students;');
-var_dump($statement->fetchAll());
+$studentDataList = $statement->fetchAll(pdo::FETCH_ASSOC);
+$studentList = [];
+//var_dump($statement->fetchAll(pdo::FETCH_CLASS, Student::class)); // as propriedades da clasee igual as colunas do banco
+
+foreach ($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['ID'],
+        $studentData['NAME'],
+        new \DateTimeImmutable($studentData['BIRTH_DATE'])
+    );
+}
+
+var_dump($studentDataList);
