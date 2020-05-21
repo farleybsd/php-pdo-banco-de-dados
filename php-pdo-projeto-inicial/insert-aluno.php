3d81722c66564c760f1204b8fe1c18abb175243e
farleybsd <farley.t.i@hotmail.com>
2020-05-21 14:03:25 -0300

    bindValue vs bindParam

--- php-pdo-projeto-inicial/insert-aluno.php
+++ php-pdo-projeto-inicial/insert-aluno.php
@@ -9,16 +9,19 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(
     null,
-    'Vinicus Dias',
-    new \DateTimeImmutable('1997-10-15')
+    'Farley De Souza Rudino',
+    new \DateTimeImmutable('1992-01-05')
 );
 
-$sqlInsert = "INSERT INTO students(NAME,BIRTH_DATE) VALUES('{$student->name()}','{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students(NAME,BIRTH_DATE) VALUES(:name,:birth_date);";
+$statemnet = $pdo->prepare($sqlInsert);
+$statemnet->bindValue(':name', $student->name());
+$statemnet->bindValue('birth_date', $student->birthDate()->format('Y-m-d'));
 
-echo $sqlInsert;
+if ($statemnet->execute()) {
+    echo 'Aluno incluido';
+}
+echo $sqlInsert . PHP_EOL;
 
-echo 'Execultando no Banco Hein!!!' . PHP_EOL;
 
-var_dump($pdo->exec($sqlInsert));
-
-echo 'EXECULTOUUU!!';
+//var_dump($pdo->exec($sqlInsert));
