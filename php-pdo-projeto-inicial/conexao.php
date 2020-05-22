1deca0c608d2a7493a6ef8e66ae3c91eec6aac49
farleybsd <farley.t.i@hotmail.com>
2020-05-22 17:43:00 -0300

    Criando o telefone

--- php-pdo-projeto-inicial/conexao.php
+++ php-pdo-projeto-inicial/conexao.php
@@ -5,11 +5,21 @@ $pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo 'Conectado';
 
-$pdo->exec('
+$CREATETABLE = '
 
-    CREATE TABLE students(ID INTEGER PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS students(ID INTEGER PRIMARY KEY,
         NAME TEXT,
         BIRTH_DATE TEXT
 );
 
-');
+    CREATE TABLE IF NOT EXISTS phone(
+        ID INTEGER PRIMARY KEY,
+        AREA_CODE TEXT,
+        NUMBER TEXT,
+        STUDENT_ID INTEGER,
+        FOREIGN KEY(STUDENT_ID) REFERENCES students(ID)
+    );
+
+
+';
+$pdo->exec($CREATETABLE);
--- php-pdo-projeto-inicial/src/Domain/Model/Student.php
+++ php-pdo-projeto-inicial/src/Domain/Model/Student.php
@@ -7,6 +7,7 @@ class Student
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    private array $phone = [];
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +50,15 @@ class Student
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhones(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    public function phones()
+    {
+        return $this->phone;
     }
 }
